dc31bdbb6a5d379c047caa9c7f712f1087fc374d
daeho KIm <kng02068@naver.com>
2026-06-25 21:32:41 +0900

    fix:

--- apps/server/src/controllers/index.ts
+++ apps/server/src/controllers/index.ts
@@ -1,7 +1,3 @@
 import { EDITOR_CONTROLLERS } from "./editor";
-import { SDK_CONTROLLERS } from "./sdk";
 
-export const APP_CONTROLLERS = [
-  ...EDITOR_CONTROLLERS,
-  ...SDK_CONTROLLERS,
-];
+export const APP_CONTROLLERS = [...EDITOR_CONTROLLERS];
