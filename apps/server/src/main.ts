7761efae2db960373eba080c125149d6e52f17f2
daeho KIm <kng02068@naver.com>
2026-07-02 23:21:51 +0900

    fix:

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from "./app.module";
 import { ConfigService } from "@nestjs/config";
 import { setupExceptionFilter } from "./bootstrap/setup-exception-filter";
 import { setupOpenapi } from "./bootstrap/setup-openapi";
-import { setupValidation } from "./bootstrap/setu-validation";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,7 +12,6 @@ async function bootstrap() {
   const port = Number(config.get("SERVER_PORT") ?? 3000);
 
   setupOpenapi(app);
-  setupValidation(app);
   setupExceptionFilter(app);
 
   await app.listen(port);
